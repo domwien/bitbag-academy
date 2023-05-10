c0f432ac5ae5e4a48e1e742e34f2cad1d739db83
domwien <dominik.wiench@outlook.com>
2023-05-10 20:46:24 +0200

    Modify 'Product' entity via adding colors

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -16,6 +16,34 @@ use Sylius\Component\Product\Model\ProductTranslationInterface;
 #[ORM\Table(name: 'sylius_product')]
 class Product extends BaseProduct
 {
+    /** @ORM\Column(name="color", type="string", nullable=true) */
+    #[ORM\Column(name: "color", type: "string", nullable: true)]
+    private $color;
+
+    public const COLOR_RED = 'Red';
+    public const COLOR_GREEN = 'Green';
+    public const COLOR_BLUE = 'Blue';
+
+    public static function showColors()
+    {
+        return [
+            self::COLOR_RED => 'red',
+            self::COLOR_GREEN => 'green',
+            self::COLOR_BLUE => 'blue',
+        ];
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function setColor($color): void
+    {
+        $this->color = $color;
+    }
+
+
     protected function createTranslation(): ProductTranslationInterface
     {
         return new ProductTranslation();
